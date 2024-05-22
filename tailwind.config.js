c2034711ab8e53d20d6d435c4ee8f0f0df27dfb0
Farman <140155090+FarManCoder003@users.noreply.github.com>
2024-05-22 21:45:43 +0600

    Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    maxWidth:{
-      container:"1320px"
+    extend: {
+      maxWidth: {
+        container: "1320px",
+      },
+      fontFamily: {
+        pops: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        banbg: "url('./src/assets/images/Banner.png')",
+        serone: "url('./src/assets/images/Services1.png')",
+        sertwo: "url('./src/assets/images/Services2.png')",
+        serthree: "url('./src/assets/images/Services3.png')",
+        aboutimg: "url('./src/assets/images/About.png')",
+        blog1: "url('./src/assets/images/Blog 1.png')",
+        blog2: "url('./src/assets/images/Blog 2.png')",
+        blog3: "url('./src/assets/images/Blog 3.png')",
+      },
     },
-    fontFamily: {
-      'pops':['Poppins', 'sans-serif']
-    },
-    backgroundImage: {
-      'banbg': "url('./src/assets/images/Banner.png')",
-      'serone': "url('./src/assets/images/Services1.png')",
-      'sertwo': "url('./src/assets/images/Services2.png')",
-      'serthree': "url('./src/assets/images/Services3.png')",
-      'aboutimg': "url('./src/assets/images/About.png')",
-      'blog1': "url('./src/assets/images/Blog 1.png')",
-      'blog2': "url('./src/assets/images/Blog 2.png')",
-      'blog3': "url('./src/assets/images/Blog 3.png')"
-    }
   },
   plugins: [],
-}
+};
